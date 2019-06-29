616fbe0070e2aded5a3ab336fa36653d05e091fe
Belendia Abdissa <belendia@gmail.com>
2019-06-29 16:05:56 +0300

    Add functionality to update projects

--- src/actions/projectActions.js
+++ src/actions/projectActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { GET_ERRORS, GET_PROJECTS } from "./types";
+import { GET_ERRORS, GET_PROJECTS, GET_PROJECT } from "./types";
 
 export const createProject = (project, history) => {
   return dispatch => {
@@ -7,6 +7,10 @@ export const createProject = (project, history) => {
       .post("http://localhost:8080/api/project", project)
       .then(response => {
         history.push("/dashboard");
+        dispatch({
+          type: GET_ERRORS,
+          payload: {} // if it is good data and saved, wipe out all the previously stored errors.
+        });
       })
       .catch(error => {
         dispatch({
@@ -25,5 +29,25 @@ export const getProjects = () => {
         payload: response.data
       });
     });
+  };
+};
+
+export const getProject = (id, history) => {
+  return dispatch => {
+    axios
+      .get(`http://localhost:8080/api/project/${id}`)
+      .then(response => {
+        dispatch({
+          type: GET_PROJECT,
+          payload: response.data
+        });
+      })
+      .catch(error => {
+        // dispatch({
+        //   type: GET_ERRORS,
+        //   payload: error.response.data
+        // });
+        history.push("/dashboard");
+      });
   };
 };
