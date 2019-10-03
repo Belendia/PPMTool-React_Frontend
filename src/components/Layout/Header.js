cc246fdd2650f89b21ec3ac90040e77957a88beb
Belendia Abdissa <belendia@gmail.com>
2019-10-03 16:03:42 +0300

    Update header component with security

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -5,7 +5,58 @@ import { connect } from "react-redux";
 import { logout } from "../../actions/securityActions";
 
 class Header extends Component {
+  logout = () => {
+    this.props.logout();
+    window.location = "/";
+  };
+
   render() {
+    const { validToken, user } = this.props.security;
+    const userIsAuthenticated = (
+      <div className="collapse navbar-collapse" id="mobile-nav">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link className="nav-link" to="/dashboard">
+              Dashboard
+            </Link>
+          </li>
+        </ul>
+
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <Link className="nav-link " to="/dashboard">
+              <i className="fas fa-user-circle mr-1">{user.fullName}</i>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/logout" onClick={this.logout}>
+              Logout
+            </Link>
+          </li>
+        </ul>
+      </div>
+    );
+
+    const userNotAuthenticated = (
+      <div className="collapse navbar-collapse" id="mobile-nav">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <Link className="nav-link " to="/register">
+              Sign Up
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/login">
+              Login
+            </Link>
+          </li>
+        </ul>
+      </div>
+    );
+
+    let headerLinks =
+      validToken && user ? userIsAuthenticated : userNotAuthenticated;
+
     return (
       <nav className="navbar navbar-expand-sm navbar-dark bg-primary mb-4">
         <div className="container">
@@ -21,32 +72,31 @@ class Header extends Component {
             <span className="navbar-toggler-icon" />
           </button>
 
-          <div className="collapse navbar-collapse" id="mobile-nav">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item">
-                <Link className="nav-link" to="/dashboard">
-                  Dashboard
-                </Link>
-              </li>
-            </ul>
-
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <Link className="nav-link " to="/register">
-                  Sign Up
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/login">
-                  Login
-                </Link>
-              </li>
-            </ul>
-          </div>
+          {headerLinks}
         </div>
       </nav>
     );
   }
 }
 
-export default Header;
+Header.propTypes = {
+  logout: PropTypes.func.isRequired,
+  security: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => {
+  return {
+    security: state.security
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    logout: () => dispatch(logout())
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Header);
