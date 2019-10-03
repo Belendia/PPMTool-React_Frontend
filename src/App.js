509b3dc0dd80880e06bd7f822f55dc7a8c05ea10
Belendia Abdissa <belendia@gmail.com>
2019-10-03 15:32:45 +0300

    Add login functionality

--- src/App.js
+++ src/App.js
@@ -14,6 +14,26 @@ import { Provider } from "react-redux";
 import store from "./store";
 import Register from "./components/UserManagement/Register";
 import Login from "./components/UserManagement/Login";
+import jwt_decode from "jwt-decode";
+import setJWTToken from "./utils/setJWTToken";
+import { SET_CURRENT_USER } from "./actions/types";
+import { logout } from "./actions/securityActions";
+
+const jwtToken = localStorage.jwtToken;
+if (jwtToken) {
+  setJWTToken(jwtToken);
+  const decoded_token = jwt_decode(jwtToken);
+  store.dispatch({
+    type: SET_CURRENT_USER,
+    payload: decoded_token
+  });
+  const currentTime = Date.now() / 1000;
+  if (decoded_token.exp < currentTime) {
+    store.dispatch(logout());
+    //this.props.history.push("/");
+    window.location.href = "/";
+  }
+}
 
 function App() {
   return (
--- src/actions/securityActions.js
+++ src/actions/securityActions.js
@@ -1,5 +1,7 @@
 import axios from "../axios-ppm";
-import { GET_ERRORS } from "./types";
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
+import setJWTToken from "../utils/setJWTToken";
+import jwt_decode from "jwt-decode";
 
 export const createNewUser = (newUser, history) => {
   return dispatch => {
@@ -25,5 +27,68 @@ export const createNewUser = (newUser, history) => {
           });
         }
       });
+  };
+};
+
+/*
+ * Steps
+ * ----------------------
+ * post => Login Request
+ * Extract token from response.data
+ * store the token in the localStorage
+ * set our token in header
+ * decode token on React
+ * dispatch to our securityReducer
+ */
+
+export const login = loginRequest => {
+  return dispatch => {
+    axios
+      //post => Login Request
+      .post("/api/users/login", loginRequest)
+      .then(response => {
+        //Extract token from response.data
+        const { token } = response.data;
+
+        //store the token in the localStorage
+        localStorage.setItem("jwtToken", token);
+
+        //set our token in header
+        setJWTToken(token);
+
+        //decode token on React
+        const decoded_token = jwt_decode(token);
+
+        //dispatch to our securityReducer
+        dispatch({
+          type: SET_CURRENT_USER,
+          payload: decoded_token
+        });
+      })
+      .catch(error => {
+        if (error.response === undefined) {
+          dispatch({
+            type: GET_ERRORS,
+            payload: error
+          });
+        } else {
+          dispatch({
+            type: GET_ERRORS,
+            payload: error.response.data
+          });
+        }
+      });
+  };
+};
+
+export const logout = () => {
+  localStorage.removeItem("jwtToken");
+  setJWTToken(false); // delete the Authorization from axios header
+
+  return dispatch => {
+    dispatch({
+      type: SET_CURRENT_USER,
+      payload: {}
+    });
   };
 };
